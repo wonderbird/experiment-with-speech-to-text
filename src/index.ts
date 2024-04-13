b196deb08a94b3a102d3f0668b1c225efcd54fc2
Stefan Boos <kontakt@boos.systems>
2024-04-13 22:19:35 +0200

    feat: transcribe audio file
    following the guide at https://platform.openai.com/docs/guides/speech-to-text/quickstart?lang=node

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-export class Greeter {
-  greet() {
-    return "Hello World!";
-  }
+import fs from "fs";
+import OpenAI from "openai";
+import 'dotenv/config';
+
+const openai = new OpenAI();
+
+async function main() {
+  const transcription = await openai.audio.transcriptions.create({
+    file: fs.createReadStream("./speech.mp3"),
+    model: "whisper-1",
+  });
+
+  console.log(transcription.text);
 }
 
-console.log(new Greeter().greet());
-
+console.log(`Using OpenAI API key "${process.env.OPENAI_API_KEY_NAME}"`);
+main();
